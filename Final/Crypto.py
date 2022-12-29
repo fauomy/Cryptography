d47e3c19b5b0a0173dee28f00d3f96fe9e1e2568
fauomy <92949869+fauomy@users.noreply.github.com>
2022-12-29 20:20:56 +0200

    Update Crypto.py

--- Final/Crypto.py
+++ Final/Crypto.py
@@ -1,11 +1,13 @@
+import binascii
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from Crypto import Random
 from tkinter.messagebox import showinfo, showerror
+
 recordsConnection = sqlite3.connect('records.db')
+
 
 # Designing Main Screen So, first of all, you have to design the main screen.
 # two buttons Login and Register.
@@ -15,7 +17,7 @@ def main_screen():
             addUserQuery = "INSERT INTO tbl_user (name,username,password) VALUES (?, ?, ?)"
             user_data = (name, username, password)
             recordsConnection.execute(addUserQuery, user_data)
-            recordsConnection.commit();
+            recordsConnection.commit()
 
         def signUpClick():
             if usernameEntry.get() != "" and nameEntry.get() != "" and passwordEntry.get() != "":
@@ -28,21 +30,18 @@ def main_screen():
                     nameEntry.delete(0, END)
                     usernameEntry.delete(0, END)
                     passwordEntry.delete(0, END)
-                    signUpScreen.destroy()
-                    main_screen()
-                    mainscreen.mainloop()
                 else:
                     showerror("ERROR", 'User already exists.')
                 recordsConnection.commit()
             else:
-                messagebox.showerror("Error", "Please Fill the fields appropriately (Password must be 8 characters at least).")
+                messagebox.showerror("Error",
+                                     "Please Fill the fields appropriately (Password must be 8 characters at least).")
 
-##sign up screen
+        ##sign up screen
         def closeClick():
             signUpScreen.destroy()
             main_screen()
             mainscreen.mainloop()
-
 
         mainscreen.destroy()
         signUpScreen = Tk()  # create a GUI window
@@ -73,9 +72,11 @@ def main_screen():
         passwordEntry.grid(row=2, column=1, padx=10, pady=10)
 
         # Use the padx and pady options to add some padding around the buttons
-        btnSignUp = Button(signUpScreen, text="SignUp", command=signUpClick, font=("Helvetica", 14), fg='black', bg='green', padx=20, pady=10)
+        btnSignUp = Button(signUpScreen, text="SignUp", command=signUpClick, font=("Helvetica", 14), fg='black',
+                           bg='green', padx=20, pady=10)
         btnSignUp.grid(row=3, column=0, padx=10, pady=10)
-        btnClose = Button(signUpScreen, text="Close", command=closeClick, font=("Helvetica", 14), fg='black', bg='red', padx=20, pady=10)
+        btnClose = Button(signUpScreen, text="Close", command=closeClick, font=("Helvetica", 14), fg='black', bg='red',
+                          padx=20, pady=10)
         btnClose.grid(row=3, column=1, padx=10, pady=10)
 
         signUpScreen.mainloop()
@@ -86,61 +87,60 @@ def main_screen():
                 addMessageQuery = "INSERT INTO tbl_messages (id,input,encrypted_text) VALUES (?, ?, ?)"
                 message_data = (id, input, encrypted_text)
                 recordsConnection.execute(addMessageQuery, message_data)
-                recordsConnection.commit();
+                recordsConnection.commit()
 
             def encrypt():
-                # Get the input message and PGP private key from the text fields
                 message = input_text.get('1.0', 'end')
-                private_key = pgp_private_key_text.get('1.0', 'end')
-                # Generate a random AES key
-                aes_key = Random.new().read(AES.block_size)
-                # Encrypt the message with the AES key
-                aes_cipher = AES.new(aes_key, AES.MODE_EAX)
-                ciphertext, tag = aes_cipher.encrypt_and_digest(message.encode())
-                # Create a new encryption object for the RSA key
-                encryptor = PKCS1_OAEP.new(RSA.importKey(private_key))
-                # Encrypt the AES key with the RSA key
-                encrypted_aes_key = encryptor.encrypt(aes_key)
-                # Concatenate the encrypted AES key, tag, and ciphertext into a single bytes object
-                encrypted_message = encrypted_aes_key + tag + ciphertext
-                # Convert the encrypted message to a hexadecimal string
-                encrypted_message_hex = encrypted_message.hex()
+                print(message)
+                encryptor = PKCS1_OAEP.new(pubKey)
+                encrypted = encryptor.encrypt(message.encode())
+                print("Encrypted:", binascii.hexlify(encrypted))
                 # Insert the encrypted message into the encrypted output text field
                 encrypted_output_text.delete('1.0', 'end')
-                encrypted_output_text.insert('1.0', encrypted_message_hex)
-                addMessageToDb(user_id, message, encrypted_message_hex)
-                showinfo("Message","The input has been encrypted successfully!")
+                encrypted_output_text.insert('1.0', (binascii.hexlify(encrypted)))
+                addMessageToDb(user_id, message, (binascii.hexlify(encrypted)))
+                showinfo("Message", "The input has been encrypted successfully!")
 
             def decrypt():
-                # Get the encrypted input and PGP public key from the text fields
+                decryptor = PKCS1_OAEP.new(keyPair)
+                # Get the encrypted input and PGP private key from the text fields
                 encrypted_input = encrypted_input_text.get('1.0', 'end')
-                public_key = pgp_private_key_text.get('1.0', 'end')
-
-                # Convert the encrypted input from a hexadecimal string to a bytes object
-                encrypted_input_bytes = bytes.fromhex(encrypted_input)
-
-                # Create a new decryption object for the RSA public key
-                decryptor = PKCS1_OAEP.new(RSA.importKey(public_key))
-
-                # Decrypt the AES key
-                aes_key = decryptor.decrypt(encrypted_input_bytes[:256])
-
-                # Extract the tag and ciphertext from the encrypted message
-                tag = encrypted_input_bytes[256:288]
-                ciphertext = encrypted_input_bytes[288:]
-
-                # Create a new decryption object for the AES key
-                aes_cipher = AES.new(aes_key, AES.MODE_EAX, tag)
-
-                # Decrypt the message
+                print("encrypt in is", encrypted_input)
+                byte_data = bytes.fromhex(encrypted_input)
+                print("byte data", byte_data)
                 try:
-                    decrypted_message = aes_cipher.decrypt(ciphertext)
-                except ValueError:
-                    decrypted_message = b"Invalid PGP Public Key"
+                    decrypted = decryptor.decrypt(byte_data)
+                    print('Decrypted:', decrypted)
 
-                # Insert the decrypted message into the decrypted output text field
-                decrypted_output_text.delete('1.0', 'end')
-                decrypted_output_text.insert('1.0', decrypted_message.decode('utf-8'))
+                    # Insert the decrypted message into the decrypted output text field
+                    decrypted_output_text.delete('1.0', 'end')
+                    decrypted_output_text.insert('1.0', decrypted)
+                except:
+                    showerror("Error", 'Invalid Encrypted String.')
+
+            def readme():
+                red = Tk()
+                screen_width = red.winfo_screenwidth()
+                screen_height = red.winfo_screenheight()
+                window_width = 500
+                window_height = 500
+                x_coordinate = (screen_width / 2) - (window_width / 2)
+                y_coordinate = (screen_height / 2) - (window_height / 2)
+                red.config(bg='light blue')
+                red.title("Read me")
+                red.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))
+                textArea = Text(red, font="lucida 13")
+                textArea.pack(expand=True, fill=BOTH)
+                textArea.insert(END,"First of all I generated keypair from the RSA key and from the pair I generated "
+                                    "public key and private key. \nEncryption:\n\t For Encryption I take the input "
+                                    "from the user and encode it, then I generated a key with the help of Public key, "
+                                    "and encrypted the input string with the help of generated key and then convert "
+                                    "the encrypted string into the readale  hex format with the help of "
+                                    "binascii.hexlify.\nDecryption:\n\t I take the encrypted string from the user and "
+                                    "convert it into bytes from hex format, Generate the key with the help of private "
+                                    "key form at the first step. the decrypte the byte format to original string. One "
+                                    "thing keep in mind that if the private key is changed your data can't be "
+                                    "decrypted at anny cost make sure it stays same.")
 
             loginScreen.destroy()
             cryptoScreen = Tk()
@@ -148,38 +148,40 @@ def main_screen():
             cryptoScreen.title("Encryption/Decryption Machine")
             screen_width = cryptoScreen.winfo_screenwidth()
             screen_height = cryptoScreen.winfo_screenheight()
-            window_width = 1000
-            window_height = 650
+            window_width = 1200
+            window_height = 800
             x_coordinate = (screen_width / 2) - (window_width / 2)
             y_coordinate = (screen_height / 2) - (window_height / 2)
             cryptoScreen.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))
 
             # Create the label and text widget for the PGP private key
-            pgp_private_key_label = Label(cryptoScreen, text="PGP Private Key", font=("Helvetica", 14),bg='light blue')
+            pgp_private_key_label = Label(cryptoScreen, text="PGP Private Key", font=("Helvetica", 14))
             pgp_private_key_label.grid(row=0, column=0, padx=10, pady=10)
-            pgp_private_key_text = Text(cryptoScreen, height=20, width=50, bd=5, bg='white', font=("Helvetica", 14),fg='black', padx=10, pady=10)
+            pgp_private_key_text = Text(cryptoScreen, height=20, width=50, bd=5, bg='white', font=("Helvetica", 14),
+                                        fg='black', padx=10, pady=10)
             pgp_private_key_text.grid(row=0, column=1, padx=10, pady=10)
 
             # Create the label and text widget for the input
-            input_label = Label(cryptoScreen, text="Input                   ", justify=LEFT,bg='light blue')
-            input_text = Text(cryptoScreen, height=1, width=100, bd=5)
+            input_label = Label(cryptoScreen, text="Input")
+            input_text = Text(cryptoScreen, height=1, width=100, bg='white', font=("Helvetica", 14), fg='black',
+                              padx=10, pady=10)
             input_label.grid(row=1, column=0, padx=10, pady=10)
             input_text.grid(row=1, column=1, padx=10, pady=10)
 
             # Create the label and text widget for the encrypted output
-            encrypted_output_label = Label(cryptoScreen, text="Encrypted Output",bg='light blue')
+            encrypted_output_label = Label(cryptoScreen, text="Encrypted Output")
             encrypted_output_text = Text(cryptoScreen, height=1, width=100, bd=5)
             encrypted_output_label.grid(row=2, column=0, padx=10, pady=10)
             encrypted_output_text.grid(row=2, column=1, padx=10, pady=10)
 
             # Create the label and text widget for the encrypted input
-            encrypted_input_label = Label(cryptoScreen, text="Encrypted Input ",bg='light blue')
+            encrypted_input_label = Label(cryptoScreen, text="Encrypted Input")
             encrypted_input_text = Text(cryptoScreen, height=1, width=100, bd=5)
             encrypted_input_label.grid(row=3, column=0, padx=10, pady=10)
             encrypted_input_text.grid(row=3, column=1, padx=10, pady=10)
 
             # Create the label and text widget for the decrypted output
-            decrypted_output_label = Label(cryptoScreen, text="Decrypted Output",bg='light blue')
+            decrypted_output_label = Label(cryptoScreen, text="Decrypted Output")
             decrypted_output_text = Text(cryptoScreen, height=1, width=100, bd=5)
             decrypted_output_label.grid(row=4, column=0, padx=10, pady=10)
             decrypted_output_text.grid(row=4, column=1, padx=10, pady=10)
@@ -189,13 +191,14 @@ def main_screen():
             decrypt_button = Button(cryptoScreen, text="Decrypt", command=decrypt)
             encrypt_button.grid(row=5, column=1, columnspan=2, padx=10, pady=10)
             decrypt_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+            Button(cryptoScreen,text='About Machine',command=readme,font='Bahnschrift 11 bold').place(x=20, y=10)
+            keyPair = RSA.generate(3072)
 
-            # Generate RSA key pair
-            key = RSA.generate(2048)
-            public_key = key.publickey().exportKey()
-            private_key = key.exportKey()
-            # Insert the private key into the appropriate text box
-            pgp_private_key_text.insert('1.0', private_key)
+            pubKey = keyPair.publickey()
+            pubKeyPEM = pubKey.exportKey()
+            privKeyPEM = keyPair.exportKey()
+            # # Insert the private key into the appropriate text box
+            pgp_private_key_text.insert('1.0', privKeyPEM.decode('ascii'))
             cryptoScreen.mainloop()
 
         def validateUser(name, password):
@@ -203,12 +206,12 @@ def main_screen():
             cur.execute("""SELECT * FROM tbl_user WHERE username=? and password=?""", (name, password,))
             row = cur.fetchone()
             if row is None:
-                showerror("Error",'Invalid username or password.')
+                showerror("Error", 'Invalid username or password.')
             else:
                 user_id = row[0]
                 usernameEntry.delete(0, END)
                 passwordEntry.delete(0, END)
-                showinfo("Message","Logged in successfully")
+                showinfo("Message", "Logged in successfully")
                 openCryptoForm(user_id)
             recordsConnection.commit()
 
@@ -217,8 +220,9 @@ def main_screen():
                 validateUser(usernameEntry.get(), passwordEntry.get())
 
             else:
-                showerror("Error","Please Fill the fields appropriately (Password must be 8 characters at least).")
-### login screen
+                showerror("Error", "Please Fill the fields appropriately (Password must be 8 characters at least).")
+
+        ### login screen
         def closeClick():
             loginScreen.destroy()
             main_screen()
@@ -243,14 +247,16 @@ def main_screen():
         LblPassword.grid(row=1, column=0, padx=10, pady=10)
         passwordEntry = Entry(loginScreen, bd=5)
         passwordEntry.grid(row=1, column=1, padx=10, pady=10)
-        btnLogin = Button(loginScreen, text="Login", command=loginClick, font=("Helvetica", 14), fg='black', bg='green', padx=20, pady=10)
+        btnLogin = Button(loginScreen, text="Login", command=loginClick, font=("Helvetica", 14), fg='black', bg='green',
+                          padx=20, pady=10)
         btnLogin.grid(row=2, column=1, padx=10, pady=10)
-        btnClose = Button(loginScreen, text="Close", command=closeClick, font=("Helvetica", 14), fg='black', bg='red', padx=20, pady=10)
+        btnClose = Button(loginScreen, text="Close", command=closeClick, font=("Helvetica", 14), fg='black', bg='red',
+                          padx=20, pady=10)
         btnClose.grid(row=2, column=0, padx=10, pady=10)
 
         loginScreen.mainloop()
 
-###the main page pop up window
+    ###the main page pop up window
 
     mainscreen = Tk()  # create a GUI window
     screen_width = mainscreen.winfo_screenwidth()
@@ -259,11 +265,16 @@ def main_screen():
     window_height = 400
     x_coordinate = (screen_width / 2) - (window_width / 2)
     y_coordinate = (screen_height / 2) - (window_height / 2)
-    mainscreen.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))  # set the configuration of GUI window
+    mainscreen.geometry("%dx%d+%d+%d" % (
+        window_width, window_height, x_coordinate, y_coordinate))  # set the configuration of GUI window
     mainscreen.title("Welcome")  # set the title of GUI window
     mainscreen.config(bg='light blue')  # Change the background color to light blue
-    Button(mainscreen, text="Login", height="2", width="30", command=showLoginFrame, font=("Helvetica", 14), fg='black',bg='light green', padx=20, pady=10).pack(padx=10, pady=10)
-    Button(mainscreen, text="Register", height="2", width="30", command=showSignUpFrame, font=("Helvetica", 14),fg='black', bg='red', padx=20, pady=10).pack(padx=10, pady=10)
+    Button(mainscreen, text="Login", height="2", width="30", command=showLoginFrame, font=("Helvetica", 14), fg='black',
+           bg='light green', padx=20, pady=10).pack(padx=10, pady=10)
+    Button(mainscreen, text="Register", height="2", width="30", command=showSignUpFrame, font=("Helvetica", 14),
+           fg='black', bg='red', padx=20, pady=10).pack(padx=10, pady=10)
     mainscreen.mainloop()  # start the GUI
 
+
 main_screen()  # call the main_account_screen() function
+
